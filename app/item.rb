a0601526db88c7c90d9f77b9290b3e5ea54af460
Casey Van-Roose <caseyvanroose@Caseys-MacBook-Pro.local>
2018-08-04 17:16:59 +0100

    Done.

--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,15 @@
 class Item
   attr_accessor :name, :price
 
+  @@items = []
+
   def initialize(name,price)
     @name = name
     @price = price
+    @@items <<  self
   end
+
+  Item.new("cup", 50.00)
+  Item.new("plate",  25.25)
+
 end
